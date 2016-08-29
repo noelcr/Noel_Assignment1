a1aea814d1c0642a4858b3695e387e14768d2b5f
Noel <noel.chillerroberts@my.jcu.edu.au>
2016-08-29 21:41:13 +1000

    Setting up files and prop

--- Main/src/Card.java
+++ Main/src/Card.java
@@ -1,5 +1,8 @@
 public class Card {
     public static void main(String []args){
-        
+        int cardTotal = 54;
+        int numOfPlayers;
+        int playerHand = 8;
     }
+
 }
---
+++ Main/src/Player.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Noel on 29/08/2016.
+ */
+public class Player {
+
+    public void play(){
+
+    }
+}
